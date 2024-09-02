a9df0ccdb1c4a5a5430d105903bde0a95e576990
Mukesh <billamukesh1234@gmail.com>
2024-09-02 22:26:28 +0530

    update: HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-const HomePage = () => {
+import prisma from "@/lib/prisma";
+import { Product } from "@/utils/types";
+
+const HomePage = async () => {
+  const products: Product[] = await prisma.product.findMany();
+
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Welcome to Mini E-commerce</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 export default HomePage;
